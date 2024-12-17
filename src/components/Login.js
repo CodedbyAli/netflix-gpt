e9bb3ad3341380b2cd338e9eee661eaf5e5b8f87
Ali Hamza <quotedbyali@gmail.com>
2024-12-18 00:29:27 +0500

    Toggle between signin and signup page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,14 @@
+import { useState } from "react";
+
+
 const Login = () => {
+
+    const [isSigninForm,setIsSigninForm] = useState(true);
+
+    const toggleSignInForm = () => {
+        setIsSigninForm(!isSigninForm);
+    }
+
     return (
       <>
         <div className="relative w-screen h-screen">
@@ -17,19 +27,22 @@ const Login = () => {
           <div className="absolute w-full h-full flex items-end justify-center z-10">
                   <div className="bg-black rounded-lg w-4/12 bg-opacity-60">
                               <div className="p-16">
-                                  <h1 className="text-white text-3xl font-bold pb-4">Sign In</h1>
+                                  <h1 className="text-white text-3xl font-bold pb-4">{isSigninForm ? 'Sign In' : 'Sign Up'}</h1>
                                   <form>
+                                      {!isSigninForm && <input type="text" placeholder="Email or phone number" className="p-4 bg-black my-4 rounded-sm text-white w-full block border border-zinc-600 bg-opacity-80" />}
                                       <input type="text" placeholder="Email or phone number" className="p-4 bg-black my-4 rounded-sm text-white w-full block border border-zinc-600 bg-opacity-80 bg-[rgba(0,0,0,0.85)]" />
                                       <input type="text" placeholder="Password" className="p-4 bg-black my-4 rounded-sm w-full block border border-zinc-600 bg-opacity-85 bg-[rgba(0,0,0,0.85)]" />
-                                      <button type="submit" className="text-white bg-red-600 font-semibold rounded-sm w-full p-2 mt-4">Sign In</button>
+                                      <button type="submit" className="text-white bg-red-600 font-semibold rounded-sm w-full p-2 mt-4">{isSigninForm ? 'Sign In' : 'Sign Up'}</button>
                                       <h3 className="text-zinc-400 my-4 text-center">OR</h3>
-                                      <button type="submit" className="bg-white text-white font-semibold rounded-sm w-full p-2 bg-opacity-30">Use a Sign-In Code</button>
-                                      <div className="flex justify-center my-4"><a href="/" className="text-white">Forgot Password?</a></div>
+                                      <button type="submit" className="bg-white text-white font-semibold rounded-sm w-full p-2 bg-opacity-30">{isSigninForm ? 'Use a Sign-In Code' : 'Use a Sign-In Code'}</button>
+                                      {isSigninForm && <div className="flex justify-center my-4"><a href="/" className="text-white">Forgot Password?</a></div>}
                                       <div className="flex items-center space-x-2 my-4">
                                           <input type="checkbox" id="rememberMe" className="h-5 w-5 border-2 border-zinc-600 rounded-sm focus:ring-0"/>
                                           <label htmlFor="rememberMe" className="text-white text-md cursor-pointer">Remember me</label>
                                       </div>
-                                      <p className="text-zinc-400">New to Netflix?<span className="text-zinc-100 font-semibold ml-2"><a href="/">Sign up now</a></span></p>
+                                      {isSigninForm ? <p className="text-zinc-400">New to Netflix?<span onClick={toggleSignInForm} className="text-zinc-100 font-semibold ml-2 cursor-pointer hover:underline">Sign up now</span></p>
+                                       :
+                                      <p className="text-zinc-400">Already a member?<span onClick={toggleSignInForm} className="text-zinc-100 font-semibold ml-2 cursor-pointer hover:underline">Sign in instead</span></p>}
                                       <p className="text-zinc-500 mt-4">This page is protected by Google reCAPTCHA to ensure you're not a bot. <span className="text-blue-500">Learn more.</span></p>
                                   </form>
   
